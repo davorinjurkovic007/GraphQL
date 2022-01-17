13dd93f60f68d6efa2dd9297e232c2d7bf9f9ff7
davorinjurkovic007 <davorin.jurkovic@gmail.com>
2022-01-17 16:29:21 +0100

    GraphQL Hello World start example

---
+++ GraphQLSeries/Hello.GraphQL/HelloworldQuery.cs
@@ -0,0 +1,28 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello.GraphQL
+{
+    internal class HelloworldQuery : ObjectGraphType
+    {
+        public HelloworldQuery()
+        {
+            Field<StringGraphType>(
+                name: "hello",
+                resolve: context => "World"
+                );
+
+            Field<StringGraphType>(
+                name: "test",
+                resolve: context => "This is a test");
+
+            Field<StringGraphType>(
+                name: "howdy",
+                resolve: context => "universe");
+        }
+    }
+}
---
+++ GraphQLSeries/Hello.GraphQL/Program.cs
@@ -0,0 +1,52 @@
+﻿using GraphQL;
+using GraphQL.NewtonsoftJson;
+using GraphQL.Types;
+using System;
+using System.Threading.Tasks;
+
+namespace Hello.GraphQL
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            var schemaFirst = Schema.For(@"
+                type Query {
+                    hello : String
+                    }
+                ");
+
+            var schemaFirstJson = await schemaFirst.ExecuteAsync(_ =>
+            {
+                _.Query = "{ hello }";
+                _.Root = new { Hello = "World" };
+            });
+
+            Console.WriteLine("Schema First: "+schemaFirstJson);
+
+            /* Code first approach */
+            var codeFirst = new Schema { Query = new HelloworldQuery() };
+
+            var codeFirstJson = await codeFirst.ExecuteAsync(_ =>
+            {
+                _.Query = "{" +
+                "   howdy" +
+                "   hello" +
+                "   test" +
+                "}";
+            });
+
+            var secondCodeFirstJson = await codeFirst.ExecuteAsync(_ =>
+            {
+                _.Query = "{test}";
+            });
+
+            Console.WriteLine("Code first: " + codeFirstJson);
+
+            Console.WriteLine("This is second test: " + secondCodeFirstJson);
+
+        }
+    }
+}
