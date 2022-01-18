96575d534da762902e43e222c0bb03f6d3cb9037
davorinjurkovic007 <davorin.jurkovic@gmail.com>
2022-01-18 16:25:20 +0100

    Demo VedaVesum who working

---
+++ GraphQLAzureDemo/GraphQLAzureDemo/Notes/Note.cs
@@ -0,0 +1,8 @@
+﻿namespace GraphQLAzureDemo.Notes
+{
+    public class Note
+    {
+        public Guid Id { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ GraphQLAzureDemo/GraphQLAzureDemo/Notes/NotesSchema.cs
@@ -0,0 +1,12 @@
+﻿using GraphQL.Types;
+
+namespace GraphQLAzureDemo.Notes
+{
+    public class NotesSchema : Schema
+    {
+        public NotesSchema(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+            Query = serviceProvider.GetRequiredService<NotesQuery>();
+        }
+    }
+}
