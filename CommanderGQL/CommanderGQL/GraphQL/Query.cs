f48d6a5af2cf953f06db64e8e4599bdb7c375cac
davorinjurkovic007 <davorin.jurkovic@gmail.com>
2022-01-24 00:02:02 +0100

    Adding projections

---
+++ CommanderGQL/CommanderGQL/GraphQL/Query.cs
@@ -0,0 +1,18 @@
+﻿using CommanderGQL.Data;
+using CommanderGQL.Models;
+using HotChocolate;
+using HotChocolate.Data;
+using System.Linq;
+
+namespace CommanderGQL.GraphQL
+{
+    public class Query
+    {
+        [UseDbContext(typeof(AppDbContext))]
+        [UseProjection]
+        public IQueryable<Platform> GetPlatform([ScopedService] AppDbContext context)
+        {
+            return context.Platforms;
+        }
+    }
+}
--- CommanderGQL/CommanderGQL/Startup.cs
+++ CommanderGQL/CommanderGQL/Startup.cs
@@ -1,21 +1,39 @@
+using CommanderGQL.Data;
+using CommanderGQL.GraphQL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GraphQL.Server.Ui.Voyager;
 
 namespace CommanderGQL
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddPooledDbContextFactory<AppDbContext>(opt => opt.UseSqlServer(configuration.GetConnectionString("CommandConnectionString")));
+
+            services
+                .AddGraphQLServer()
+                .AddQueryType<Query>()
+                .AddProjections();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,11 +48,10 @@ namespace CommanderGQL
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapGraphQL();
             });
+
+            app.UseGraphQLVoyager("/graphql-voyager");
         }
     }
 }
