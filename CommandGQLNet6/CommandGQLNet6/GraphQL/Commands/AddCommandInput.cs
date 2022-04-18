c642fdc17b65c5649c7065945b2c657d1e37add7
davorinjurkovic007 <davorin.jurkovic@gmail.com>
2022-04-18 09:22:19 +0200

    AddCommand

---
+++ CommandGQLNet6/CommandGQLNet6/GraphQL/Commands/AddCommandInput.cs
@@ -0,0 +1,4 @@
+﻿namespace CommandGQLNet6.GraphQL.Commands
+{
+    public record AddCommandInput(string HowTo, string CommandLine, int PlatformId);
+}
---
+++ CommandGQLNet6/CommandGQLNet6/GraphQL/Commands/AddCommandPayload.cs
@@ -0,0 +1,4 @@
+﻿namespace CommandGQLNet6.GraphQL.Commands
+{
+    public record AddCommandPayload();
+}
